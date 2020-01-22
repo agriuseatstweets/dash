4ece72605c02fd132ea7ea8cbe07ada24fb13464
Nandan <nandanmarkrao@gmail.com>
2020-01-22 09:40:11 +0100

    logstash mapping and tweet format tweaks

--- logstash/prep_tweet.rb
+++ logstash/prep_tweet.rb
@@ -31,9 +31,21 @@ def _trim_subtweet(event, subkey, keys)
   event.set(subkey, s.slice(*keys))
 end
 
+def _clean_user(event)
+  user = event.get("user")
+  return unless user
+  keys = user.keys().select{ |k| !k.include? "profile" }
+  event.set("user", user.slice(*keys))
+end
+
 def filter(event)
   if !event.get("[limit][track]").nil?
     event.tag("limit")
+    return [event]
+  end
+
+  if !event.get("delete").nil?
+    event.tag("delete")
     return [event]
   end
 
@@ -44,14 +56,14 @@ def filter(event)
     ["entities", "truncated", "extended_tweet"].each do |e|
       _set_from(event, e, "[retweeted_status][#{e}]")
     end
-    _trim_subtweet(event, "retweeted_status", ["user", "id_str"])
+    _trim_subtweet(event, "retweeted_status", ["user", "id_str", "created_at"])
   end
 
   # quote tweets -> keep entities separate... TODO: get extended tweet from quoted_status!
   if event.get("quoted_status")
     _handle_truncated(event, "[quoted_status]")
     _handle_entities(event, "[quoted_status]")
-    _trim_subtweet(event, "quoted_status", ["user", "id_str", "entities", "text"])
+    _trim_subtweet(event, "quoted_status", ["user", "id_str", "entities", "text", "created_at"])
   end
 
   # handle truncated values & entities
@@ -59,7 +71,16 @@ def filter(event)
   _handle_entities(event)
   event.remove("extended_tweet")
 
+  # clean user
+  _clean_user(event)
+
   [event]
+end
+
+test "user: profile fields dropped" do
+  in_event {{ "user" => { "id" => 123, "lang" => "en", "profile_banner_url" => "foo"}}}
+  expect("banner url is gone") {|events| events.first.get("[user][profile_banner_url]").nil?}
+  expect("everything else still there") {|events| events.first.get("[user][lang]") == "en"}
 end
 
 test "when limit exists" do
@@ -68,13 +89,13 @@ test "when limit exists" do
 end
 
 test "user mentions flattened" do
-  in_event { { "entities" => { "user_mentions" => [{"screen_name" => "foo"}, {"screen_name" => "bar"}] } } }
+  in_event { {"user" => {}, "entities" => { "user_mentions" => [{"screen_name" => "foo"}, {"screen_name" => "bar"}] } } }
   expect("only names in user_mentions") {|events|  events.first.get("[entities][user_mentions]") == ["foo", "bar"]}
   expect("no hashtags") {|events|  events.first.get("[entities][hashtags]").nil?}
 end
 
 test "hashtags flattend" do
-  in_event { { "entities" => { "hashtags" => [{"text" => "foo"}, {"text" => "bar"}] } } }
+  in_event { { "user" => {}, "entities" => { "hashtags" => [{"text" => "foo"}, {"text" => "bar"}] } } }
   expect("only names in hash tags") do |events|
     events.first.get("[entities][hashtags]") == ["foo", "bar"]
   end
@@ -85,6 +106,7 @@ end
 
 test "flattening works with extended values" do
   in_event { {
+               "user" => {},
                "truncated" => true,
                "text" => "foo",
                "entities" => {"hashtags" => [{"text": "foo"}]},
@@ -102,6 +124,7 @@ end
 
 test "retweeted status" do
   in_event { {
+               "user" => {},
                "truncated" => true,
                "text" => "foo",
                "entities" => {"hashtags" => [{"text": "foo"}]},
@@ -112,7 +135,8 @@ test "retweeted status" do
                "retweeted_status" => {
                  "user" => { "screen_name" => "matt", "nope" => "gone" },
                  "truncated" => true,
-                 "id_str" => "qux",
+                 "created_at" => "Mon Apr 17 13:53:38 +0000 2017",
+                 "id_str" => "123",
                  "entities" => {"hashtags" => [{"text": "foo"}]},
                  "extended_tweet" => {
                    "full_text" => "foobarbaz",
@@ -147,8 +171,9 @@ test "quoted status" do
                },
                "quoted_status" => {
                  "user" => { "screen_name" => "matt", "nope" => "gone" },
+                 "created_at" => "Mon Apr 17 13:53:38 +0000 2017",
                  "truncated" => true,
-                 "id_str" => "qux",
+                 "id_str" => "123",
                  "entities" => {"hashtags" => [{"text": "foo"}]},
                  "extended_tweet" => {
                    "full_text" => "foobarbaz",
